b89d78fc9e0c6ff21dc0267f5bb4196755d4188b
mansanael <magathnael@meltwater.org>
2022-10-16 18:48:59 +0000

    DayOfTheWeek Class
    Create a class and implement dayOfTheWeek method with dynamic binding

---
+++ DayOfTheWeek.java
@@ -0,0 +1,36 @@
+public class DayOfTheWeek {
+       public static void main(String[] args) {
+                
+       }
+       
+       String dayOfTheWeek(int D) {
+        String theDay = "";
+        switch (D) {
+            case 1:
+                theDay = "Monday";
+                break;
+            case 2:
+                theDay = "Thuesday";
+                break;
+            case 3:
+                theDay = "Wednesday";
+                break;
+            case 4:
+                theDay = "Thursday";
+                break;
+            case 5:
+                theDay = "Friday";
+                break;
+            case 6:
+                theDay = "Saturday";
+                break;
+            case 7:
+                theDay = "Sunuday";
+                break;
+            default:
+                break;
+
+        }
+        return theDay;
+    }
+}
--- TryMethods.java
+++ TryMethods.java
@@ -6,39 +6,11 @@ public class TryMethods {
      * @param args
      */
     public static void main(String[] args) {
-        String myDay = dayOfTheWeek(4);
-        System.out.println(myDay);
-
+        DayOfTheWeek myDay = new DayOfTheWeek();
+        String theDay = myDay.dayOfTheWeek(3); 
+        System.out.println(theDay);
+        
     }
 
-    public static String dayOfTheWeek(int D) {
-        String theDay = "";
-        switch (D) {
-            case 1:
-                theDay = "Monday";
-                break;
-            case 2:
-                theDay = "Thuesday";
-                break;
-            case 3:
-                theDay = "Wednesday";
-                break;
-            case 4:
-                theDay = "Thursday";
-                break;
-            case 5:
-                theDay = "Friday";
-                break;
-            case 6:
-                theDay = "Saturday";
-                break;
-            case 7:
-                theDay = "Sunuday";
-                break;
-            default:
-                break;
-
-        }
-        return theDay;
-    }
+    
 }
