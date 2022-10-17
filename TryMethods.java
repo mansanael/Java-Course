85a882420082b169297cc5fb240a592ba033d372
mansanael <magathnael@meltwater.org>
2022-10-17 09:07:22 +0000

    DayOfTheWeek deleted and method added to TryMethods

--- TryMethods.java
+++ TryMethods.java
@@ -1,3 +1,4 @@
+
 public class TryMethods {
     /**
      * This methods is the TryMethods
@@ -6,10 +7,42 @@ public class TryMethods {
      * @param args
      */
     public static void main(String[] args) {
-        DayOfTheWeek myDay = new DayOfTheWeek();
+        TryMethods myDay = new TryMethods();
+
         String theDay = myDay.dayOfTheWeek(3); 
         System.out.println(theDay);
         
+    }
+
+    String dayOfTheWeek(int D) {
+        String theDay = "";
+        switch (D) {
+            case 1:
+                theDay = "Monday";
+                break;
+            case 2:
+                theDay = "Tuesday";
+                break;
+            case 3:
+                theDay = "Wednesday";
+                break;
+            case 4:
+                theDay = "Thursday";
+                break;
+            case 5:
+                theDay = "Friday";
+                break;
+            case 6:
+                theDay = "Saturday";
+                break;
+            case 7:
+                theDay = "Sunuday";
+                break;
+            default:
+                break;
+
+        }
+        return theDay;
     }
 
     
