9d42326bcf74d17bde1112c1f43caaf1938b0f72
mansanael <magathnael@meltwater.org>
2022-10-17 09:36:30 +0000

    Basic If Statement done
    Mood finder basic

--- TryMethods.java
+++ TryMethods.java
@@ -9,7 +9,20 @@ public class TryMethods {
         DayOfTheWeek myDay = new DayOfTheWeek();
         String theDay = myDay.dayOfTheWeek(3); 
         System.out.println(theDay);
+
+        TryMethods Mood  = new TryMethods();
+        Mood.ifStatementMood("Happy");
         
+    }
+
+    public void ifStatementMood (String mood){
+        if (mood == "Happy"){
+            System.out.println("This guy is happy ! Hourra !");
+        } else if (mood == "Sad") {
+            System.out.println("Sorry ! Enjoy life my guy !");
+        }else {
+            System.out.println("No Mood !");
+        }
     }
 
     
